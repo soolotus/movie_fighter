f93dffee6a482ce4e9cb5ac960c1d2989e997093
SooYeon Seong <soolotus632@gmail.com>
2024-09-21 07:17:07 -0600

    updating styles

--- index.js
+++ index.js
@@ -65,7 +65,25 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 };
 
 const runComparison = () => {
-  console.log("time for comparison");
+  const leftSideStats = document.querySelectorAll(
+    "#left-summary .notification"
+  );
+  const rightSideStats = document.querySelectorAll(
+    "#right-summary .notification"
+  );
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+    const leftSideValue = parseInt(leftStat.dataset.value)
+    const rightSideValue = parseInt(rightStat.dataset.value)
+
+    if(leftSideValue > rightSideValue) {
+      leftStat.classList.remove('is-primary')
+      leftStat.classList.add('is-warning')
+    } else {
+      rightStat.classList.remove('is-primary')
+      rightStat.classList.add('is-warning')
+  });
 };
 
 const movieTemplate = (movieDetail) => {
